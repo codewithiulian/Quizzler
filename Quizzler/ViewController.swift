e8f0c126734e7e79975a31a3cc3217a34a4bc401
Iulian Oana <iulian.andreioana@gmail.com>
2019-07-30 17:07:29 +0100

    Initial app setup

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Iulian Oana on 30/07/2019.
+//  Copyright (c) 2019 Iulian Oana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
